71f2af6837e5a3370edd9de34bed296b694ebcad
BDA824 <ferneyavila73@gmail.com>
2026-03-30 06:50:19 -0500

    Add Customer interface-domain

--- src/main/java/com/project/domain/model/ICustomerRepository.java
+++ src/main/java/com/project/domain/model/ICustomerRepository.java
@@ -1,6 +1,12 @@
 package com.project.domain.model;
-import reactor.core.publisher.Mono;
+import reactor.core.publisher.Mono
 
-public class ICustomerRepository {
+import java.util.List;
 
+public interface ICustomerRepository {
+
+    Mono<customerEntity> create(customerEntity cl);
+    Mono<customerEntity> findCustomerById(int id);
+    Mono<List<customerEntity>> getAllCustomers();
+    Mono<customerEntity> updateCustomer(customerEntity cl);
 }
